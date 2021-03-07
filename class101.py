1c40228c5680352bc20dd6240f1b0cfd9ca01dde
Ubuntu <ubuntu@ip-172-31-33-203.ap-northeast-2.compute.internal>
2021-03-07 19:54:23 +0900

    class101.py: modified

--- class101.py
+++ class101.py
@@ -6,6 +6,11 @@ import datetime
 import pandas as pd
 start  = time.time()
 
+
+def rmEmoji(inputData):
+    return inputData.encode('utf-8', 'ignore').decode('utf-8').encode('ascii', 'ignore').decode('ascii')
+
+
 offset = 28 * 0
 limit = 28 * 38
 url_graphql = 'https://cdn-gql-prod2.class101.net/graphql'
@@ -22,58 +27,81 @@ for txt in req.text.split('\n'):
         prohibit_url.append('https://class101.net' + txt.replace('Disallow: ', ''))
         
 
-class101_df = pd.DataFrame(columns=['url_detail', 'url', 'title', 'category_1', 'category_2', 'state', 'o_price', 's_price', 'teacher', 'teacher_nick', 'feedback_count', 'feedback_good', 'reservation', 'heart'])
+class101_df = pd.DataFrame(columns=['site', 'link', 'title', 'teacher', 'category_1', 'category_2', 's_price', 'discount', 'contentment'])
 
 for cat_ko, cat_eng, brand in categories:
     print(f'{cat_ko} / {cat_eng}')
     query = [{"operationName":"InfiniteProductCardsWithLastUpdatedInformation","variables":{"brand":[brand],"offset":offset,"limit":limit},"query":"query InfiniteProductCardsWithLastUpdatedInformation($brand: [ProductBrand!], $limit: Int, $offset: Int, $categoryIds: [String!]) {\n  products(\n    limit: $limit\n    offset: $offset\n    productFilter: {brand: $brand, isHidden: false, isLastManagement: true, state: [funding, sales], type: \"klass\", categoryIds: $categoryIds}\n    sort: [{managedAt: -1}]\n  ) {\n    ...ProductCardWithLastUpdatedInformation\n    __typename\n  }\n  productsCount(\n    productFilter: {brand: $brand, categoryIds: $categoryIds, isHidden: false, isLastManagement: true, state: [funding, sales], type: \"klass\"}\n  )\n}\n\nfragment ProductCardWithLastUpdatedInformation on Product {\n  _id\n  categoryId\n  categoryIds\n  coverImageUrl\n  firestoreId\n  title\n  titlePrefixes\n  type\n  state\n  packagePricePreview {\n    listPrice\n    netPrice\n    __typename\n  }\n  ...CategoryAndCreatorTag\n  ...ProductBadge\n  ...LastUpdatedInformation\n  ...HeartAndFeedbackCountLabel\n  __typename\n}\n\nfragment CategoryAndCreatorTag on Product {\n  _id\n  category {\n    _id\n    title\n    __typename\n  }\n  categoryTitleDetail\n  difficulty\n  author {\n    _id\n    firestoreId\n    name\n    nickName\n    photoUrl\n    channels {\n      type\n      url\n      channelId\n      __typename\n    }\n    createdAt\n    __typename\n  }\n  __typename\n}\n\nfragment ProductBadge on Product {\n  productBadge {\n    text\n    imageUrl\n    backgroundColor\n    fontColor\n    __typename\n  }\n  __typename\n}\n\nfragment LastUpdatedInformation on Product {\n  _id\n  lastManagement {\n    managedAt\n    content\n    __typename\n  }\n  __typename\n}\n\nfragment HeartAndFeedbackCountLabel on Product {\n  _id\n  feedbackCount\n  feedbackGoodCount\n  reservationCount\n  wishlistedCount\n  __typename\n}\n"}]
-    req = requests.post(url_graphql, json=query)
+    req = requests.post(url_graphql, json=query, )
     datas = req.json()
 
 
     onetime = []
     for i in range(len(datas[0]['data']['products'])):
+        
+        site = '클래스101'
+        
         url_detail = datas[0]['data']['products'][i]['_id']
-        url = 'https://class101.net/products/' + url_detail
-        if url in prohibit_url:
+        link = 'https://class101.net/products/' + url_detail
+        if link in prohibit_url:
             continue
-        title = datas[0]['data']['products'][i]['title']
+            
+        state = datas[0]['data']['products'][i]['state']
+        title = '[' + state + ']' + datas[0]['data']['products'][i]['title']
+        
         category_1 = cat_ko
         category_2 = datas[0]['data']['products'][i]['category']['title']
-        state = datas[0]['data']['products'][i]['state']
+
         try:
-            o_price = datas[0]['data']['products'][i]['packagePricePreview']['listPrice']
+            o_price_int = datas[0]['data']['products'][i]['packagePricePreview']['listPrice']
+            o_pirce = '총 ' + str(f'{o_price_int:,}') + '원'
         except:
             o_price = '링크 참조'
-            print('o_price', url, i)
+            print('o_price', link, i)
         try:
-            s_price = datas[0]['data']['products'][i]['packagePricePreview']['netPrice']
+            s_price_int = datas[0]['data']['products'][i]['packagePricePreview']['netPrice']
+            s_price = '총 ' + str(f'{s_price_int:,}') + '원'
         except:
             s_price = '링크 참조'
-            print('s_price', url, i)
-        teacher = datas[0]['data']['products'][i]['author']['name']
+            print('s_price', link, i)
+        try:
+            discount = round((s_price_int / o_price_int) * 100, 1) + '%'
+        except:
+            discount = '없음'
+            
         teacher_nick = datas[0]['data']['products'][i]['author']['nickName']
-        feedback_count = datas[0]['data']['products'][i]['feedbackCount']
-        feedback_good = datas[0]['data']['products'][i]['feedbackGoodCount']
-        reservation = datas[0]['data']['products'][i]['reservationCount']
-        heart = datas[0]['data']['products'][i]['wishlistedCount']
+        teacher_real = datas[0]['data']['products'][i]['author']['name']
+        if teacher_nick != None:
+            if teacher_real != None:
+                teacher = teacher_nick + '[' + datas[0]['data']['products'][i]['author']['name'] + ']'
+            else:
+                teacher = teacher_nick
+        else:
+            teacher = teacher_real
+            
+        feedback_count = int(datas[0]['data']['products'][i]['feedbackCount'])
+        feedback_good = int(datas[0]['data']['products'][i]['feedbackGoodCount'])
+        if feedback_count != 0:
+            contentment = str(round((feedback_good / feedback_count) * 100, 1)) + '%'
+        else:
+            contentment = '0'
+#         reservation = datas[0]['data']['products'][i]['reservationCount']
+#         heart = datas[0]['data']['products'][i]['wishlistedCount']
 
 
         row = {
-            'url_detail': url_detail, 
-            'url': url,
-            'title': title, 
-            'state': state, 
+#             'url_detail': url_detail, 
+            'site': site,
+            'link': link,
+            'title': rmEmoji(title),
+            'teacher': rmEmoji(teacher),
             'category_1': category_1,
             'category_2': category_2,
-            'o_price': o_price, 
-            's_price': s_price, 
-            'teacher': teacher, 
-            'teacher_nick': teacher_nick,
-            'feedback_count': feedback_count, 
-            'feedback_good': feedback_good, 
-            'reservation': reservation, 
-            'heart': heart
+            's_price': s_price,
+            'discount': discount,
+            'contentment': contentment,
+#             'reservation': reservation, 
+#             'heart': heart
             }
         onetime.append(row)
         
