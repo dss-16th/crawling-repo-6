e03e57dcf959974c394a44b959cdeade8f2ac5c2
wisdome743911 <wisdome743911@gmail.com>
2021-03-10 13:20:00 +0900

    path, modifying

--- classtok.py
+++ classtok.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 
 import requests
 from bs4 import BeautifulSoup
@@ -12,33 +11,16 @@ start  = time.time()
 
 url = 'https://www.classtok.net/modoo/start_now'
 response = requests.get(url)
-response
-
-
-
 soup = BeautifulSoup(response.content, 'html.parser')
-soup
-
-
-
 elements = soup.select('body > div.main_wrap > div > div > ul > div > li > div > a.product_info')
-len(elements)
-
-
 
 links = []
 for i in range(len(elements)):
     links.append('https://classtok.net' + str(elements[i]).replace('<a class="product_info" href="', '').split('">\n<span>')[0])
-len(links)
 
-
-
-#site, title, category_2, teacher, price, discount, likes, link
-
-
+classtok_df = pd.DataFrame(columns=['site', 'link', 'title', 'teacher', 'category_1', 'category_2', 's_price', 'discount', 'contentment'])
 
 onetime = []
-datas = []
 
 for element in elements:
     
@@ -48,29 +30,24 @@ for element in elements:
     except:
         likes = ''
     
-    datas.append({
+    onetime.append({
         "site" : site,
         "title" : element.select_one('h2').text,
         "category_2" : element.select_one('span').text.split(' · ')[0],
         "teacher" : element.select_one('span').text.split(' · ')[1],
-        "price" : element.select_one('.price_info').text.split('\n')[1],
+        "s_price" : element.select_one('.price_info').text.split('\n')[1],
         "discount" : element.select_one('.price_info').text.split('\n')[2].replace(' 할인',''),
-        "likes" : likes,
+        "contentment" : likes,
         "link" : 'https://www.classtok.net' + element.get('href'),
     })
-    onetime.append(datas)
-classtok_df = pd.DataFrame(datas)
-classtok_df.tail()
-
-
-
 classtok_df = classtok_df.append(onetime)
+
 print('time: ', round((time.time() - start)/60, 1), '분', sep='')
 print('\n')
 
 
 
-classtok_df.to_csv(f'./classtok_{datetime.datetime.now().strftime("%y%m%d%H%M%S")}.csv', encoding='utf-8')
+classtok_df.to_csv(f'./datas/classtok_{datetime.datetime.now().strftime("%y%m%d%H%M%S")}.csv', encoding='utf-8')
 
 
 print('전체')
